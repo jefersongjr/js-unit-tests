f251c3669a34e04ffc4b915891c775767c564f0c
jefersongjr <jeferson_gjr@hotmail.com>
2022-04-09 00:55:09 -0300

    requisito 7 - implementando a funçao ObjPlayground

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,8 +33,29 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  const operacoes = {
+    sum: number1 + number2,
+    mult: number1 * number2,
+    div: Math.floor(number1 / number2),
+    sub: number1 - number2,
+  };
 
-const arrayGenerator = (type, object) => {};
+  return operacoes;
+};
+
+const arrayGenerator = (type, object) => {
+  let array = [];
+  if (type === 'keys') {
+    array = Object.keys(object);
+  } else if (type === 'values') {
+    array = Object.values(object);
+  } else if (type === 'entries') {
+    array = Object.entries(object);
+  } else {
+    console.log('undefined');
+  }
+  return array;
+};
 
 module.exports = { calculator, arrayGenerator };
